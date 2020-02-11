085beed9a98c1cb6261bb87a6efc0f1f31419ac1
Anand Natu <natu.anand@gmail.com>
2020-02-10 16:36:14 -0800

    Added closed-form solution for Merton Problem

--- portfolio_opt/portfolio_opt.py
+++ portfolio_opt/portfolio_opt.py
@@ -3,7 +3,7 @@ import re
 import sys
 sys.path.append('../')
 from MDP import util
-
+import numpy as np
 
 # See https://en.wikipedia.org/wiki/Merton%27s_portfolio_problem
 
@@ -23,8 +23,9 @@ class MertonProblem():
 		self.rho = 0.7
 		# Risk-free rate
 		self.r = 0.9
-		# Number of time steps
+		# Total life time
 		self.T = 1000
+
 
 		# Current time step
 		self.time = 0
@@ -39,10 +40,27 @@ class MertonProblem():
     		return (c^(1-gamma))/(1-gamma) 
 
 
-    # Closed-form optimal split of the
-    def optimalSplit(self):
-    	# Declare the optimal policy
+    # Closed-form optimal solution derived analytically
+    def closedFormSolution(self):
+    	# Declare epsilon parameter
+    	eps = 1e-6
+
+    	# Optimal split fraction between risky and riskless asset
     	pi_opt = (self.mu - self.r)/(1-self.gamma)
 
+    	# Compute parameter
+    	nu = (self.rho/self.gamma) - (1-self.gamma)*(((self.mu-self.r)*pi_opt/2*self.gamma)+(self.r/self.gamma))
+
+    	# Compute the wealth fraction
+    	if self.time == self.T:
+    		c_opt = nu
+    	elif nu == 0:
+    		c_opt = (self.T-self.time-eps)^(-1)
+    	else:
+    		c_opt = (nu*(1+(nu*eps-1)*np.exp(-nu*(self.T-self.time)))^(-1)
 
 
+
+
+if __name__ == "main":
+	mp = MertonProblem()
