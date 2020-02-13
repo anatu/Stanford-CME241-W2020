8c997f32b948f688954a45ad68f852eb41a15230
Anand Natu <natu.anand@gmail.com>
2020-02-13 12:17:44 -0800

    Added grid world + starter code for Monte Carlo, and utility functions for Derivatives Pricing

--- financial_applications/black_scholes.py
+++ financial_applications/black_scholes.py
@@ -6,9 +6,64 @@ import sys
 sys.path.append('../')
 from MDP import util
 
+# Math adapted from https://www.investopedia.com/terms/b/blackscholes.asp
+'''
+Model for pricing and hedging of derivatives in complete and incomplete markets
+The complete market assumption means no transaction costs, and a price for every asset (i.e. one can always transact
+any asset class at any point in time subject to its FMV)
+'''
 
 
+'''State = 
+t = current point in time
+Alpha = Hedge positions (units held) at time t
+P = prices per unit of hedges at time t
+Beta = PnL position at time t
+D = Portfolio state amongst m derivatives
 
 
+Action = a (units of hedges traded at price P)
+'''
+
+class HedgePricing:
+
+	def __init__(self):
+		'''
+		Initialize parameters for the problem
+		'''
+		self.T = 100
+		self.t = 0
+		self.alpha = 0
+		self.beta = 0
+		self.D = 1
+
+	def PnL_update(self, a):
+		'''
+		Update the P&L position for each time step
+		Beta_new = Beta_old Plus...
+		+ Cashflows from holding positions (X + alpha*Y)
+		- Cost of units of hedges purchased (a*P)
+		- Transaction costs (e.g. -gamma*P*abs(a), we will use this)
+		'''
+		# Evolve price and return data (TODO: Where do we get a simulator for this?)
+		self.X = np.random.normal(0,1)
+		self.Y = np.random.normal(5,1)
+		self.P = np.random.normal(4,3)
+
+		# Update the portfolio
+		self.beta = self.beta + self.X + (self.alpha*self*Y) - (a*self.P) - (self.gamma*self.P*abs(a))
 
 
+	def utilityFunc(self, beta):
+		'''
+		Calculates reward as utility of consumption from the
+		existing portfolio state
+		'''
+		# CRRA coefficient (relative risk-aversion)
+		coeff = 0.5
+		result = np.exp(beta^(1-coeff) - 1)/(1-coeff)
+		return result
+
+	# def step(self):
+	# 	state = (self.t, self.alpha, self.P, self.beta, self.D)
+		
---
+++ monte_carlo/mc_demo.py
@@ -0,0 +1,14 @@
+# https://github.com/omerbsezer/Reinforcement_learning_tutorial_with_demo/blob/master/monte_carlo_demo.ipynb
+# Monte-Carlo example using Grid World game
+
+import numpy as np
+from gridWorld import standard_grid, negative_grid,print_values, print_policy
+
+# Initialize parameters
+tol = 1e-4
+gamma = 0.7
+actions = ("U", "D", "L", "R")
+
+
+
+
